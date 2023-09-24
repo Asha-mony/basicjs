3c996741d22a0d670f58b24e490d24facad7b49a
Asha-mony <ynomasha5@gmail.com>
2023-09-25 05:08:10 +0600

    first step

---
+++ basic.js
@@ -0,0 +1,44 @@
+// variables
+// string
+var fst = 'ASHA';
+var lst = 'MONY';
+var fullname = fst + ' ' + lst ;
+console.log(fullname);
+
+// nmbr
+var pay1 = 30;
+var pay2 = 20;
+var total = pay1 + pay2;
+// console.log(total);
+
+// boolean
+let num1 = 10;
+let num2 = 13; 
+// console.log(num1 > num2);
+
+
+// let use
+let counter = 1;
+counter=2;
+ counter++;
+ counter++;
+ counter--;
+//  console.log(counter);
+
+
+// if else condition
+// let x1= 20;
+// let x2= 30;
+
+let pass= 123456;
+let user= "ynomasha5@gmail.com"
+
+// login page
+
+if (123456 == pass && "ynomasha5@gmail.com" == user) {
+    console.log( 'Profile interface');
+}
+
+else{
+    console.log('password wrong');
+}
